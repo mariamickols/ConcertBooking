fa90043d916f870b1821c9295231de5ffb248fa7
HM\FRBUL <fredrik.bullsimonsen@hm.com>
2020-11-27 15:19:04 +0100

    Refactored the menu items

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+addmenues("myMenue")
+
 function click1(param)   {
    console.log(param)
    let ticketPrice = parseInt(document.getElementById("price").innerHTML)
@@ -7,4 +10,12 @@ function click1(param)   {
 
 function buy(){
    alert("Nice! See you at the concert")
+}
+
+function addmenues(id)   {
+      let html = ``
+      for(let i=1; i<11; i++)   {
+         html += `<button class="dropdown-item" type="button" onclick="click1(${i})">${i}</button>`
+      }
+   document.getElementById(id).innerHTML = html
 }
